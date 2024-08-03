8afc603c662716f0ee701b9b3bd17c8d1c7ee020
Lautigarcia17 <lauti1718garcia@gmail.com>
2024-08-03 15:12:01 -0300

    Add barrels

---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+export { default as AuthPage } from './AuthPage/AuthPage';
+export { default as Layout } from './Layout/Layout';
+export { default as MovieListPage } from './MovieListPage/MovieListPage';
+export { default as MoviePage } from './MoviePage/MoviePage';
+export { default as PageNotFound } from './PageNotFound/PageNotFound';
+export { default as ProfilePage } from './ProfilePage/ProfilePage';

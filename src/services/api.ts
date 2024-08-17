98e25fd35237ccee9a8c30262e39f8cebf6a9458
Lautigarcia17 <lauti1718garcia@gmail.com>
2024-08-17 12:36:28 -0300

    fix url https

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const API_URL: string = 'http://www.omdbapi.com/?apikey=5b1461ac';
+const API_URL: string = 'https://www.omdbapi.com/?apikey=5b1461ac';
 
 
 export const getMovieSearch = (value: string) => {

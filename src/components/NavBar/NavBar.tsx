f3da3f3fa4021af23aafb4f8001816f3c252b124
Lautigarcia17 <lauti1718garcia@gmail.com>
2024-12-09 09:46:41 -0300

    Adjust styles

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,6 +1,6 @@
-import { Link, NavLink } from 'react-router-dom'
+import {NavLink, useLocation, useNavigate } from 'react-router-dom'
 import styles from './NavBar.module.css'
-import logo from '../../assets/Bmovie-removebg-preview.png'
+import logo from '../../assets/Bmovie.png'
 import { authContext } from '../../context/AuthContext'
 import toast from 'react-hot-toast'
 import { useGenericContext } from "../../hooks/useGenericContext";
@@ -8,10 +8,14 @@ import { useEffect, useState } from 'react'
 import { scrollContext } from '../../context/ScrollContext'
 
 
+
 function NavBar() {
     const { session, signOut } = useGenericContext(authContext)
     const {scrollRef} = useGenericContext(scrollContext)
     const [isScrolled,setIsScrolled] = useState(false);
+    const location = useLocation();
+    const navigate = useNavigate();
+
 
     const handleLogout = async () => {
         await signOut();
@@ -31,7 +35,7 @@ function NavBar() {
         };
       
         scrollElement.addEventListener('scroll', handleScroll);
-      
+
         return () => {
           scrollElement.removeEventListener('scroll', handleScroll);
         };
@@ -39,10 +43,16 @@ function NavBar() {
 
       const handleScrollToTop = () =>{
         if (scrollRef.current) {
-            scrollRef.current.scrollTo({
-                top:0,
-                behavior: 'smooth'
-            });
+
+            if(location.pathname !== '/'){
+                navigate('/');
+            }
+            else{
+                scrollRef.current.scrollTo({
+                    top:0,
+                    behavior: 'smooth'
+                });
+            }
         }
       }
 
@@ -54,12 +64,12 @@ function NavBar() {
                 {session && (
                     <>
                         <div className={styles.logo}>
-                            <Link to='/' onClick={handleScrollToTop}>
+                            <button className={styles.buttonHome} onClick={handleScrollToTop}>
                                 <img src={logo} alt="logo" />
-                            </Link>
+                            </button>
                         </div>
                         <div className={styles.element}>
-                            <button className={styles.btnNav}><NavLink onClick={handleScrollToTop} to='/' className={({ isActive }) => `${styles.link} ${isActive ? styles.btnWhite : ''}`}> Home</NavLink></button>
+                            <button onClick={handleScrollToTop}  className={ `${styles.btnNav} ${location.pathname === '/' ? styles.btnWhite : ''}`}> Home</button>
                             <button className={styles.btnNav}>
                                     <NavLink to= '/profile'className={({ isActive }) => `${styles.link} ${isActive ? styles.btnWhite : ''}`}> 
                                         <svg xmlns="http://www.w3.org/2000/svg" className={styles.iconProfile} viewBox="0 0 24 24"><path fill="currentColor" fillRule="evenodd" d="M12 4a8 8 0 0 0-6.96 11.947A4.99 4.99 0 0 1 9 14h6a4.99 4.99 0 0 1 3.96 1.947A8 8 0 0 0 12 4m7.943 14.076A9.959 9.959 0 0 0 22 12c0-5.523-4.477-10-10-10S2 6.477 2 12a9.958 9.958 0 0 0 2.057 6.076l-.005.018l.355.413A9.98 9.98 0 0 0 12 22a9.947 9.947 0 0 0 5.675-1.765a10.055 10.055 0 0 0 1.918-1.728l.355-.413zM12 6a3 3 0 1 0 0 6a3 3 0 0 0 0-6" clipRule="evenodd"/></svg>

60101a16731c452005c16ae98dd21082d68de50a
Lautigarcia17 <lauti1718garcia@gmail.com>
2024-12-11 13:55:20 -0300

    Add DropdownNavbar

---
+++ src/components/DropdownNavbar/DropdownNavbar.tsx
@@ -0,0 +1,39 @@
+import { Dropdown } from "react-bootstrap"
+import styles from './DropdownNavbar.module.css'
+import { useNavigate } from "react-router-dom";
+
+function DropdownNavbar ({handleScrollToTop,handleLogout} : {handleScrollToTop: ()=> void, handleLogout: ()=> void}){
+
+  const navigate = useNavigate();
+
+  const handleNavigate = (path: string) => {
+    navigate(path);
+  };
+
+  return (
+    <Dropdown drop="up">
+      <Dropdown.Toggle id="dropdown-basic" className={styles.customToggle}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 32 32"><path fill="#ffffff" d="M12 4a5 5 0 1 1-5 5a5 5 0 0 1 5-5m0-2a7 7 0 1 0 7 7a7 7 0 0 0-7-7m10 28h-2v-5a5 5 0 0 0-5-5H9a5 5 0 0 0-5 5v5H2v-5a7 7 0 0 1 7-7h6a7 7 0 0 1 7 7zm0-26h10v2H22zm0 5h10v2H22zm0 5h7v2h-7z"/></svg>
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu className={styles.customMenu}>
+        <Dropdown.Item as="div" className={styles.customItem} onClick={handleScrollToTop}>
+          <svg xmlns="http://www.w3.org/2000/svg" className={styles.customIcon} viewBox="0 0 14 14"><g fill="#000000" stroke="#ffffff" strokeLinecap="round" strokeLinejoin="round"><rect width="8.5" height="13" x="2.75" y=".5" rx="1"/><path d="M5.27 7.14V3.82a.31.31 0 0 1 .47-.28l2.85 1.67a.31.31 0 0 1 0 .54L5.74 7.41a.31.31 0 0 1-.47-.27ZM2.75 10.5h8.5"/></g></svg>
+          Home
+        </Dropdown.Item>
+
+        <Dropdown.Item className={styles.customItem} onClick={()=> handleNavigate('/profile')}>
+          <svg xmlns="http://www.w3.org/2000/svg" className={styles.customIcon} viewBox="0 0 24 24"><path fill="currentColor" fillRule="evenodd" d="M12 4a8 8 0 0 0-6.96 11.947A4.99 4.99 0 0 1 9 14h6a4.99 4.99 0 0 1 3.96 1.947A8 8 0 0 0 12 4m7.943 14.076A9.959 9.959 0 0 0 22 12c0-5.523-4.477-10-10-10S2 6.477 2 12a9.958 9.958 0 0 0 2.057 6.076l-.005.018l.355.413A9.98 9.98 0 0 0 12 22a9.947 9.947 0 0 0 5.675-1.765a10.055 10.055 0 0 0 1.918-1.728l.355-.413zM12 6a3 3 0 1 0 0 6a3 3 0 0 0 0-6" clipRule="evenodd"/></svg>
+          Profile
+        </Dropdown.Item>
+
+        <Dropdown.Item className={styles.customItem} onClick={handleLogout}>
+          <svg xmlns="http://www.w3.org/2000/svg" className={styles.customIcon} viewBox="0 0 24 24"><path fill="#ffffff" d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h7v2H5v14h7v2zm11-4l-1.375-1.45l2.55-2.55H9v-2h8.175l-2.55-2.55L16 7l5 5z" /></svg>
+          Log out
+        </Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  )
+}
+
+export default DropdownNavbar
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,4 +1,4 @@
-import {NavLink, useLocation, useNavigate } from 'react-router-dom'
+import { NavLink, useLocation, useNavigate } from 'react-router-dom'
 import styles from './NavBar.module.css'
 import logo from '../../assets/Bmovie.png'
 import { authContext } from '../../context/AuthContext'
@@ -6,13 +6,14 @@ import toast from 'react-hot-toast'
 import { useGenericContext } from "../../hooks/useGenericContext";
 import { useEffect, useState } from 'react'
 import { scrollContext } from '../../context/ScrollContext'
+import DropdownNavbar from '../DropdownNavbar/DropdownNavbar'
 
 
 
 function NavBar() {
     const { session, signOut } = useGenericContext(authContext)
-    const {scrollRef} = useGenericContext(scrollContext)
-    const [isScrolled,setIsScrolled] = useState(false);
+    const { scrollRef } = useGenericContext(scrollContext)
+    const [isScrolled, setIsScrolled] = useState(false);
     const location = useLocation();
     const navigate = useNavigate();
 
@@ -23,39 +24,39 @@ function NavBar() {
     }
     useEffect(() => {
         const scrollElement = scrollRef.current;
-      
-        if (!scrollElement) return; 
-      
+
+        if (!scrollElement) return;
+
         const handleScroll = () => {
             const widthLimit = window.innerWidth < 600 ? 200 : 400;
-            if(scrollElement.scrollTop > widthLimit){
+            if (scrollElement.scrollTop > widthLimit) {
                 setIsScrolled(true);
-            }else{
+            } else {
                 setIsScrolled(false);
             }
         };
-      
+
         scrollElement.addEventListener('scroll', handleScroll);
 
         return () => {
-          scrollElement.removeEventListener('scroll', handleScroll);
+            scrollElement.removeEventListener('scroll', handleScroll);
         };
-      }, []);
+    }, []);
 
-      const handleScrollToTop = () =>{
+    const handleScrollToTop = () => {
         if (scrollRef.current) {
 
-            if(location.pathname !== '/'){
+            if (location.pathname !== '/') {
                 navigate('/');
             }
-            else{
+            else {
                 scrollRef.current.scrollTo({
-                    top:0,
+                    top: 0,
                     behavior: 'smooth'
                 });
             }
         }
-      }
+    }
 
 
 
@@ -64,24 +65,34 @@ function NavBar() {
             <div className={`${styles.content} ${isScrolled ? styles.bgScroll : styles.bgTransparent}`}>
                 {session && (
                     <>
+
+
                         <div className={styles.logo}>
                             <button className={styles.buttonHome} onClick={handleScrollToTop}>
                                 <img src={logo} alt="logo" />
                             </button>
                         </div>
                         <div className={styles.element}>
-                            <button onClick={handleScrollToTop}  className={ `${styles.btnNav} ${location.pathname === '/' ? styles.btnWhite : ''}`}> Home</button>
-                            <button className={styles.btnNav}>
-                                    <NavLink to= '/profile'className={({ isActive }) => `${styles.link} ${isActive ? styles.btnWhite : ''}`}> 
-                                        <svg xmlns="http://www.w3.org/2000/svg" className={styles.iconProfile} viewBox="0 0 24 24"><path fill="currentColor" fillRule="evenodd" d="M12 4a8 8 0 0 0-6.96 11.947A4.99 4.99 0 0 1 9 14h6a4.99 4.99 0 0 1 3.96 1.947A8 8 0 0 0 12 4m7.943 14.076A9.959 9.959 0 0 0 22 12c0-5.523-4.477-10-10-10S2 6.477 2 12a9.958 9.958 0 0 0 2.057 6.076l-.005.018l.355.413A9.98 9.98 0 0 0 12 22a9.947 9.947 0 0 0 5.675-1.765a10.055 10.055 0 0 0 1.918-1.728l.355-.413zM12 6a3 3 0 1 0 0 6a3 3 0 0 0 0-6" clipRule="evenodd"/></svg>
-                                    </NavLink>
-                            </button>   
-                            <button className={styles.btnNav} onClick={handleLogout}>
-                                <NavLink to='/auth' className={styles.link}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="#ffffff" d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h7v2H5v14h7v2zm11-4l-1.375-1.45l2.55-2.55H9v-2h8.175l-2.55-2.55L16 7l5 5z" /></svg>
-                                </NavLink>
-                            </button>
+                            {window.innerWidth > 600 ? (
+                                <>
+                                    <button onClick={handleScrollToTop} className={`${styles.btnNav} ${location.pathname === '/' ? styles.btnWhite : ''}`}> Home</button>
+                                    <button className={styles.btnNav}>
+                                        <NavLink to='/profile' className={({ isActive }) => `${styles.link} ${isActive ? styles.btnWhite : ''}`}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" className={styles.iconProfile} viewBox="0 0 24 24"><path fill="currentColor" fillRule="evenodd" d="M12 4a8 8 0 0 0-6.96 11.947A4.99 4.99 0 0 1 9 14h6a4.99 4.99 0 0 1 3.96 1.947A8 8 0 0 0 12 4m7.943 14.076A9.959 9.959 0 0 0 22 12c0-5.523-4.477-10-10-10S2 6.477 2 12a9.958 9.958 0 0 0 2.057 6.076l-.005.018l.355.413A9.98 9.98 0 0 0 12 22a9.947 9.947 0 0 0 5.675-1.765a10.055 10.055 0 0 0 1.918-1.728l.355-.413zM12 6a3 3 0 1 0 0 6a3 3 0 0 0 0-6" clipRule="evenodd" /></svg>
+                                        </NavLink>
+                                    </button>
+                                    <button className={styles.btnNav} onClick={handleLogout}>
+                                        <NavLink to='/auth' className={styles.link}>
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="#ffffff" d="M5 21q-.825 0-1.412-.587T3 19V5q0-.825.588-1.412T5 3h7v2H5v14h7v2zm11-4l-1.375-1.45l2.55-2.55H9v-2h8.175l-2.55-2.55L16 7l5 5z" /></svg>
+                                        </NavLink>
+                                    </button>
+                                </>
+                            ) :
+                                <div className={styles.dropdownNav}>
+                                    <DropdownNavbar handleScrollToTop={handleScrollToTop} handleLogout={handleLogout} />
+                                </div>
 
+                            }
                         </div>
                     </>
                 )}
